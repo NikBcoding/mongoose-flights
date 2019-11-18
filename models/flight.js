3d9564a0b10770897ef4ee0363fd6ceb9a697654
Nik Azbekyan <nikazbekyan@Nik-Azbekyans-MacBook-Pro.local>
2019-11-18 15:32:11 -0800

    first commit

---
+++ models/flight.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+var destinationSchema = new Schema({
+
+   airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SAN', 'SEA']
+    },
+    arrival: {
+        type: Date,
+    }
+});
+
+var flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United'],
+        require: true
+    },
+    flightNum: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            let today = new Date();
+            return new Date(today.setFullYear(today.getFullYear() + 1));
+         }
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SAN', 'SEA'],
+        default: 'SAN'
+    },
+
+    destinationSchema: [destinationSchema]
+
+});
+
+
+module.exports = mongoose.model('Flight', flightSchema);
